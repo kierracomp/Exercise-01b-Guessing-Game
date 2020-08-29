6dc210aa5a7ba06ee49659714911ff802046b477
kierracomp <70235976+kierracomp@users.noreply.github.com>
2020-08-29 18:17:03 -0400

    Completes the assignment

--- main2.py
+++ main2.py
@@ -1,2 +1,11 @@
 import sys, random
 assert sys.version_info >= (3,8), "This script requires at least Python 3.8"
+
+number = random.randrange(0,11)
+guess = input("Guess a number from 1 to 10: ")
+guess = int(guess)
+if guess == number:
+    print("Winner winner, chicken dinner!")
+else:
+    print("Whoops, not good enough.")
+    print("The number was " + str(number))
